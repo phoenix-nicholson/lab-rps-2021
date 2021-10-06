441cfaf5ef5d15d4ef0dd681cedc6a216f738ce3
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-10-06 15:51:29 -0700

    Needed a lot of help so not many commits, finished html and '.js'

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
+import { didUserWin, getRandomRoll } from './utils.js';
 // import functions and grab DOM elements
-
+const playButton = document.getElementById('play-btn');
+const winSpan = document.getElementById('wins');
+const lossSpan = document.getElementById('losses');
+const tieSpan = document.getElementById('ties');
 // initialize global state
+let wins = 0;
+let losses = 0;
+let ties = 0;
+// set event listeners
+playButton.addEventListener('click', ()=>{
+    const selected = document.querySelector('input[type=radio]:checked');
+    // console.log(selected);
+    const userRoll = selected.value;
+    // console.log(userRoll);
+    const actualRoll = getRandomRoll();
 
-// set event listeners 
+    if (userRoll === actualRoll){
+        ties++;
+    }
+
+    else if (didUserWin(userRoll, actualRoll)){
+        wins++;
+    } else {
+        losses++;
+    } 
+    winSpan.textContent = wins;
+    lossSpan.textContent = losses;
+    tieSpan.textContent = ties;
+// console.log('hello')
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
---
+++ utils.js
@@ -0,0 +1,26 @@
+export function getRandomRoll(){
+    return  ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random()*3)];
+    
+}
+
+
+export function didUserWin(userRoll, actualRoll){
+    console.log(userRoll, actualRoll)
+    if (userRoll === actualRoll){
+        return 'Tie!' 
+}
+else if (userRoll === 'Rock' && actualRoll === 'Scissors') {
+    return true
+}
+else if (userRoll === 'Paper' && actualRoll === 'Rock') {
+    return true
+}
+else if (userRoll === 'Scissors' && actualRoll === 'paper') {
+    return true
+}
+else {
+    return false
+}
+
+
+}
