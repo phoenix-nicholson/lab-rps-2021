8e83788e1059e63d51709f0d7368bbb8e3189099
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-10-06 18:10:08 -0700

    fixed some esl errors

--- utils.js
+++ utils.js
@@ -1,24 +1,24 @@
 export function getRandomRoll(){
-    return  ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random()*3)];
+    return ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
     
 }
 
 
 export function didUserWin(userRoll, actualRoll){
-    console.log(userRoll, actualRoll)
+    // console.log(userRoll, actualRoll)
     if (userRoll === actualRoll){
-        return 'Tie!' 
-}
-else if (userRoll === 'Rock' && actualRoll === 'Scissors') {
+        return 'Tie!' ;
+};
+     else if (userRoll === 'Rock' && actualRoll === 'Scissors') {
     return true
 }
-else if (userRoll === 'Paper' && actualRoll === 'Rock') {
+        else if (userRoll === 'Paper' && actualRoll === 'Rock') {
     return true
 }
-else if (userRoll === 'Scissors' && actualRoll === 'paper') {
+        else if (userRoll === 'Scissors' && actualRoll === 'paper') {
     return true
 }
-else {
+        else {
     return false
 }
 
